f5d962f45de0e14e47dae8b87c502d885dd16639
LLOS Lord <chungphan1607@gmail.com>
2026-06-12 14:43:34 +0700

    Update App.swift

--- PacketBlocker/App.swift
+++ PacketBlocker/App.swift
@@ -2,12 +2,9 @@ import SwiftUI
 
 @main
 struct PacketBlockerApp: App {
-    @StateObject private var vpnManager = VPNManager()
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(vpnManager)
         }
     }
 }
